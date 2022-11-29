3105061d03cd9761789057c45b3a904add701536
Sébastien Lampazona <sebastien@oclock.io>
2022-11-29 15:47:00 +0100

    feat(Dependencies): mise à jour des dépendances

--- webpack/webpack.production.js
+++ webpack/webpack.production.js
@@ -5,26 +5,33 @@ const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 const paths = require('./paths');
 const packageJSON = require('../package.json');
 
-module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-    }),
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: 'css/[name].css',
+  }),
+];
+
+if (process.env.SENTRY_AUTH_TOKEN && process.env.SENTRY_PROJECT) {
+  plugins.push(
     new SentryWebpackPlugin({
-      // sentry-cli configuration - can also be done directly through sentry-cli
-      // see https://docs.sentry.io/product/cli/configuration/ for details
+    // sentry-cli configuration - can also be done directly through sentry-cli
+    // see https://docs.sentry.io/product/cli/configuration/ for details
       authToken: process.env.SENTRY_AUTH_TOKEN,
       org: 'oclock',
       project: process.env.SENTRY_PROJECT,
       release: packageJSON.version,
-  
+
       // other SentryWebpackPlugin configuration
       include: paths.build,
       ignore: ['node_modules', 'config'],
     }),
-  ],
+  );
+}
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  plugins,
   module: {
     rules: [
       {
